cdb7cd7b63e528837ba372d49a0cb420a5c83d39
Adrian_Przychodzien <adrian.przychodzien@gmail.com>
2019-08-09 17:34:12 +0200

    feat: header component with links, some scss

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/Homepage';
-import ShopPage from './pages/shop/Shop'
+import ShopPage from './pages/shop/Shop';
+import Header from './components/header/Header'
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
