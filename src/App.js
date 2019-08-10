281294b3372c6385298b3996900f7245ccec346a
Adrian_Przychodzien <adrian.przychodzien@gmail.com>
2019-08-10 16:21:55 +0200

    feat: sign in with Google created

--- src/App.js
+++ src/App.js
@@ -4,19 +4,44 @@ import './App.css';
 import Homepage from './pages/homepage/Homepage';
 import ShopPage from './pages/shop/Shop';
 import SignIn_SignUp from './pages/SignIn_SignUp/SignIn_SignUp';
-import Header from './components/header/Header'
+import Header from './components/header/Header';
+import { auth } from './firebase/firebase.utils'
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={Homepage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignIn_SignUp} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user })
+
+      console.log(user)
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignIn_SignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
