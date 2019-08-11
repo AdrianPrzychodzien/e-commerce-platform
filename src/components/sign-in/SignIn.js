b214389e5e7ec87fadadf82968beb84f084551d5
Adrian_Przychodzien <adrian.przychodzien@gmail.com>
2019-08-11 13:36:38 +0200

    feat: finishing sign in handleSubmit method

--- src/components/sign-in/SignIn.js
+++ src/components/sign-in/SignIn.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import FormInput from '../form-input/FormInput';
 import CustomButton from '../custom-button/CustomButton';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 import './SignIn.scss'
 
 class SignIn extends Component {
@@ -14,9 +14,17 @@ class SignIn extends Component {
     }
   }
 
-  handleSubmit = e => {
+  handleSubmit = async e => {
     e.preventDefault()
-    this.setState({ email: '', password: '' })
+
+    const { email, password } = this.state
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password)
+      this.setState({ email: '', password: '' })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   handleChange = e => {
