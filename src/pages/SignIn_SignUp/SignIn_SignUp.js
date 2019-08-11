03388867e9e6631272e568c51cf2773a83d99666
Adrian_Przychodzien <adrian.przychodzien@gmail.com>
2019-08-11 13:27:48 +0200

    feat: implemented firebase utils & ability to store auth users into firestore database

--- src/pages/SignIn_SignUp/SignIn_SignUp.js
+++ src/pages/SignIn_SignUp/SignIn_SignUp.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/SignIn';
+import SignUp from '../../components/sign-up/SignUp';
 import './SignIn_SignUp.scss'
 
 const SignIn_SignUp = () => (
   <div className='SignIn_SignUp'>
     <SignIn />
+    <SignUp />
   </div>
 )
 
