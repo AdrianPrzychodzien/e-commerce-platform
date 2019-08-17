d5c3768814be9a966057d2ec0e65f216534f4e28
Adrian_Przychodzien <adrian.przychodzien@gmail.com>
2019-08-17 17:59:22 +0200

    feat: pulled out data from directory and shop_data component, made directory and shop reducers and selectors & implemented session storage thank to redux-persist library

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,26 +1,20 @@
-import React, { Component } from 'react';
-import SHOP_DATA from './ShopData';
-import CollectionPreview from '../../components/collection-preview/CollectionPreview'
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import CollectionPreview from '../../components/collection-preview/CollectionPreview';
 
-class ShopPage extends Component {
-  constructor(props) {
-    super(props);
+import { selectCollections } from '../../redux/shop/shop.selectors'
 
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
+const ShopPage = ({ collections }) => (
+  <div className='shop-page'>
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+)
 
-  render() {
-    const { collections } = this.state
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    )
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections
+})
 
-export default ShopPage
+export default connect(mapStateToProps)(ShopPage)
