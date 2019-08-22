18271f56a6359ce286dbd96e5683eac254457f13
Adrian_Przychodzien <adrian.przychodzien@gmail.com>
2019-08-22 17:31:59 +0200

    feat: Changed Redux thunk into Saga & small changes in collection-preview styles

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
-// import { persistStore } from 'redux-persist';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
-import thunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga';
+// import thunk from 'redux-thunk'
 
-const middlewares = [thunk]
+import { fetchCollectionsStart } from './shop/shop.sagas'
+
+const sagaMiddleware = createSagaMiddleware()
+
+const middlewares = [sagaMiddleware]
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger)
@@ -12,8 +17,8 @@ if (process.env.NODE_ENV === 'development') {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
-// export const persistor = persistStore(store)
+sagaMiddleware.run(fetchCollectionsStart)
 
-// export default { store, persistor }
+export const persistor = persistStore(store)
 
-export default store
+export default { store, persistor }
