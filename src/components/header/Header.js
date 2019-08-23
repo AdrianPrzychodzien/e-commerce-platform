b01e011a97051d62d816eb7638950f4636f6cc55
Adrian_Przychodzien <adrian.przychodzien@gmail.com>
2019-08-23 18:33:28 +0200

    feat: cart-sagas component added & created functionality that clears cart after signout

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { auth } from '../../firebase/firebase.utils';
+
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
+import { signOutStart } from '../../redux/user/user.actions';
+
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+
 import CartIcon from '../cart-icon/cart-icon';
 import CartDropdown from '../cart-dropdown/cart-dropdown';
-import { HeaderContainer, LogoContainer, OptionsContainer, OptionDiv, OptionLink } from './header.styles'
 
-const Header = ({ currentUser, hidden }) => (
+import {
+  HeaderContainer,
+  LogoContainer,
+  OptionsContainer,
+  OptionDiv,
+  OptionLink
+} from './header.styles'
+
+const Header = ({ currentUser, hidden, signOutStart }) => (
   <HeaderContainer>
     <LogoContainer to='/'>
       <Logo className='logo' />
@@ -19,7 +29,7 @@ const Header = ({ currentUser, hidden }) => (
       <OptionLink to='/shop'>CONTACT</OptionLink>
       {
         currentUser ?
-          <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
+          <OptionDiv onClick={signOutStart}>SIGN OUT</OptionDiv>
           :
           <OptionLink to='/signin'>SIGN IN</OptionLink>
       }
@@ -34,4 +44,8 @@ const mapStateToProps = createStructuredSelector({
   hidden: selectCartHidden,
 })
 
-export default connect(mapStateToProps)(Header)
+const mapDispatchToProps = dispatch => ({
+  signOutStart: () => dispatch(signOutStart())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
--- src/firebase/test.js
+++
@@ -1,7 +0,0 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore'
-
-const firestore = firebase.firestore()
-
-firestore.collection('users').doc('lGDy0tHAotYWanD1h0fA').collection('cartItems').doc('DbiykNu8X3l7LtoFyxYR')
-firestore.doc('users/lGDy0tHAotYWanD1h0fA/cartItems/DbiykNu8X3l7LtoFyxYR')
