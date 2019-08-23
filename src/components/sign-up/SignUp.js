b80d3d761407f3de9e3e0da894db10bab5b25e12
Adrian_Przychodzien <adrian.przychodzien@gmail.com>
2019-08-23 20:42:26 +0200

    feat: created user saga for sign up which automatically logs user in

--- src/components/sign-up/SignUp.js
+++ src/components/sign-up/SignUp.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import FormInput from '../form-input/FormInput';
 import CustomButton from '../custom-button/CustomButton';
-import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
+
+import { signUpStart } from '../../redux/user/user.actions'
+
 import './SignUp.scss'
 
 class SignUp extends React.Component {
@@ -17,6 +21,7 @@ class SignUp extends React.Component {
 
   handleSubmit = async e => {
     e.preventDefault()
+    const { signUpStart } = this.props
     const { displayName, email, password, confirmPassword } = this.state
 
     if (password !== confirmPassword) {
@@ -24,20 +29,7 @@ class SignUp extends React.Component {
       return
     }
 
-    try {
-      const { user } = await auth.createUserWithEmailAndPassword(email, password)
-
-      await createUserProfileDocument(user, { displayName })
-
-      this.setState({
-        displayName: '',
-        email: '',
-        password: '',
-        confirmPassword: ''
-      })
-    } catch (error) {
-      console.error(error)
-    }
+    signUpStart({ displayName, email, password })
   }
 
   handleChange = e => {
@@ -92,4 +84,8 @@ class SignUp extends React.Component {
   }
 }
 
-export default SignUp
+const mapDispatchToProps = dispatch => ({
+  signUpStart: (userCredentials) => dispatch(signUpStart(userCredentials))
+})
+
+export default connect(null, mapDispatchToProps)(SignUp)
