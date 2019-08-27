9eb3bd14084a222ecb944986df867f401fc7bf25
Adrian_Przychodzien <adrian.przychodzien@gmail.com>
2019-08-27 21:01:24 +0200

    Responsive design with media queries added

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,13 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
-
 import Homepage from './pages/homepage/Homepage';
 import ShopPage from './pages/shop/Shop';
 import SignInAndSignUpPage from './pages/signInSignUp/signInSignUp';
 import CheckoutPage from './pages/checkout/checkout';
-
 import Header from './components/header/Header';
+
+import { GlobalStyle } from './global.styles';
 
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions'
@@ -22,6 +21,7 @@ const App = ({ checkUserSession, currentUser }) => {
 
   return (
     <div>
+      <GlobalStyle />
       <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
