6f6c520f9543947a9dd23f7a041292230750cd74
Adrian_Przychodzien <adrian.przychodzien@gmail.com>
2019-09-01 13:07:35 +0200

    feat: tested user.sagas

--- client/src/redux/directory/directoryReducer.test.js
+++ client/src/redux/directory/directoryReducer.test.js
@@ -1,7 +1,12 @@
 import directoryReducer, { INITIAL_STATE } from './directory.reducer';
 
 describe('directoryReducer', () => {
+  // it('should return initial state', () => {
+  //   expect(directoryReducer(undefined, {})).toEqual(INITIAL_STATE);
+  // });
+
   it('should return initial state', () => {
-    expect(directoryReducer(undefined, {})).toEqual(INITIAL_STATE);
-  });
+    const mockState = { hidden: true, items: [] }
+    expect(directoryReducer(undefined, {})).toEqual(mockState)
+  })
 })
